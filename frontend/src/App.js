b08eed20a56d82d75cd11aa09ccb534089655c69
vlad <lukyanovvlad2005@gmail.com>
2023-12-14 00:31:22 +0300

    feat:
    add react

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Header } from "./components/Header";
+
+function App() {
+  const [name, setName] = useState("vlad")
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
